ad832cbe84517a0ead02daca9963ab0e32da2c78
Gbollyhub <aliasgbolly@gmail.com>
2022-04-12 15:28:58 +0100

    new changes2

--- server.js
+++ server.js
@@ -2,27 +2,26 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
-const CronJob = require('cron').CronJob;
-const Cron = require('./backup.js');
 
-const url = 'mongodb://alias:gbolly@localhost:27017/?authMechanism=DEFAULT&tls=false&authSource=lasubeb';
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection
-db.once('open', _ => {
-  console.log('Database connected:', url)
-})
 
-db.on('error', err => {
-  console.error('connection error:', err)
-})
-
-// mongoose.connect('mongodb+srv://aliasgbolly:Gbolly16@lasubebcluster.zjqmm.mongodb.net/lasubeb?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}).then( () => {
-//     console.log("Database connected")
+// const url = 'mongodb://alias:gbolly@localhost:27017/?authMechanism=DEFAULT&tls=false&authSource=lasubeb';
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connection
+// db.once('open', _ => {
+//   console.log('Database connected:', url)
 // })
 
-// mongoose.connection.on("error", err => {
-//     console.log(`DB connection error : ${ err.message }`)
+// db.on('error', err => {
+//   console.error('connection error:', err)
 // })
+
+mongoose.connect('mongodb+srv://aliasgbolly:Gbolly16@lasubebcluster.zjqmm.mongodb.net/lasubeb?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}).then( () => {
+    console.log("Database connected")
+})
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connection error : ${ err.message }`)
+})
 
 
 app.use((req, res, next) => {
@@ -51,8 +50,7 @@ app.use(ProjectRoutes)
 app.use(LogRoutes)
 app.use(PublishRoutes)
 
-// require('backup.js')
-// global.CronJob = require('./cron.js');
+
 
 const port = process.env.PORT || 4000
 
